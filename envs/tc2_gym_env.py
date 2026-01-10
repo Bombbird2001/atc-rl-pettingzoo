Fix some incorrect shapes

envs/tc2_gym_env.py
@@ -99,12 +99,12 @@ class TC2GymEnv(gym.Env):
     @staticmethod
     def _get_rewards_from_aircraft_state(aircraft_state) -> np.ndarray:
         # Also include agent ID for PettingZoo env
-        return np.array(aircraft_state).reshape(AIRCRAFT_COUNT, -1)[:,[0, 16, 18]]
+        return np.array(aircraft_state).reshape(AIRCRAFT_COUNT, -1)[:,[0, 16, 18]].astype(np.float32)
 
     @staticmethod
     def _get_terminated_from_aircraft_state(aircraft_state) -> np.ndarray:
         # Also include agent ID for PettingZoo env
-        return np.array(aircraft_state).reshape(AIRCRAFT_COUNT, -1)[:,[17, 16, 18]]
+        return np.array(aircraft_state).reshape(AIRCRAFT_COUNT, -1)[:,[17, 16, 18]].astype(np.int32)
 
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed)
@@ -174,12 +174,11 @@ class TC2GymEnv(gym.Env):
 
         # Read state, reward, terminated, truncated from shared memory
         values = self.sim_bridge.get_total_state()
-        aircraft_state = values[3 + AIRCRAFT_COUNT * 4:]
-        # print(values[3 + AIRCRAFT_COUNT * 4:])
+        aircraft_state = values[1 + AIRCRAFT_COUNT * (len(self.action_space.nvec) + 1):]
         obs = self._get_observation_from_aircraft_state(aircraft_state)
         reward = self._get_rewards_from_aircraft_state(aircraft_state)
         terminated = self._get_terminated_from_aircraft_state(aircraft_state)
-        if terminated:
+        if terminated[:,0].all():
             self.terminated_count += 1
 
         info = {}

ppo_pettingzoo_ma_tc2.py
@@ -219,21 +219,21 @@ if __name__ == "__main__":
                 terminations[step] = next_termination
                 truncations[step] = next_truncation
 
-                print(next_obs.shape)
-
                 # ALGO LOGIC: action logic
                 with torch.no_grad():
                     action, logprob, _, value = agent.get_action_and_value(next_obs[:,:,:-1])
                     print(action.shape, logprob.shape, value.squeeze(dim=-1).shape)
                     values[step] = value.squeeze(dim=-1)
-                action = action.transpose(0, 1)
+                action = action.permute((1, 2, 0))  # Reshape action to (num_envs, aircraft_count, action_dim)
                 actions[step] = action
                 logprobs[step] = logprob
 
                 # TRY NOT TO MODIFY: execute the game and log data.
                 next_obs, reward, termination, truncation, info = envs.step(
-                    action.cpu().numpy()
+                    # Concat the aircraft mask, ignores actions generated for non-existent aircraft entries
+                    torch.cat((action, next_obs[:,:,-1].to(torch.int32).unsqueeze(-1)), dim=-1).cpu().numpy()
                 )
+                print(next_obs.shape, reward.shape, termination.shape, truncation.shape)
                 rewards[step] = torch.tensor(reward).to(device).view(-1)
                 next_obs, next_termination, next_truncation = (
                     torch.Tensor(next_obs).to(device),

utils/vec_envs.py
@@ -35,14 +35,26 @@ class SequentialVecEnv(BaseVecEnv):
         self.envs = envs
         self.agent_ordering = sorted(self.envs[0].possible_agents)
 
+    def _dict_obs_to_np(self, dict_obs: dict) -> np.ndarray:
+        # Additional one/zero at the end for masking
+        return np.vstack([(np.hstack((dict_obs[agentId], np.ones(1))) if agentId in dict_obs else np.zeros(self.single_observation_space.shape)) for agentId in self.agent_ordering])
+
+    def _dict_reward_to_np(self, dict_reward: dict) -> np.ndarray:
+        return np.array([(dict_reward[agentId] if agentId in dict_reward else 0) for agentId in self.agent_ordering])
+
+    def _dict_termination_to_np(self, dict_termination: dict) -> np.ndarray:
+        return np.array([(dict_termination[agentId] if agentId in dict_termination else 0) for agentId in self.agent_ordering])
+
+    def _dict_truncation_to_np(self, dict_truncation: dict) -> np.ndarray:
+        return np.array([(dict_truncation[agentId] if agentId in dict_truncation else 0) for agentId in self.agent_ordering])
+
     def reset(self, seed: int | None = None):
         observations = []
         infos = []
 
         for env in self.envs:
             obs, info = env.reset(seed=seed)
-            # Additional one/zero at the end for masking
-            obs_np = np.vstack([(np.hstack((obs[agentId], np.ones(1))) if agentId in obs else np.zeros(self.single_observation_space.shape)) for agentId in self.agent_ordering])
+            obs_np = self._dict_obs_to_np(obs)
             observations.append(obs_np)
             infos.append(info)
 
@@ -54,8 +66,25 @@ class SequentialVecEnv(BaseVecEnv):
         if action.shape[0] != len(self.envs):
             raise ValueError(f"action.shape[0] ({action.shape[0]}) != len(self.envs) ({len(self.envs)})")
 
+        observations = []
+        rewards = []
+        terminations = []
+        truncations = []
+        infos = []
+
         for idx, env in enumerate(self.envs):
-            env.step(action[idx])
+            obs, reward, termination, truncation, info = env.step(action[idx])
+            obs_np = self._dict_obs_to_np(obs)
+            reward_np = self._dict_reward_to_np(reward)
+            termination_np = self._dict_termination_to_np(termination)
+            truncation_np = self._dict_truncation_to_np(truncation)
+            observations.append(obs_np)
+            rewards.append(reward_np)
+            terminations.append(termination_np)
+            truncations.append(truncation_np)
+            infos.append(info)
+
+        return np.stack(observations), np.vstack(rewards), np.vstack(terminations), np.array(truncations), infos
 
     def close(self):
         for env in self.envs: