dac2367c08b4620a115eab73ff561590859211ea
Shang Yi Ku <kushangyi2001@gmail.com>
2026-01-21 23:41:02 +0800

    Use separate network for critic

--- models/aircraft_agent.py
+++ models/aircraft_agent.py
@@ -15,7 +15,7 @@ class MLPAgent(nn.Module):
         super().__init__()
         # Simple MLP
         # TODO Maybe try local/neighbouring aircraft info too (with GNN)
-        self.network = nn.Sequential(
+        self.actor_latent = nn.Sequential(
             layer_init(nn.Linear(18, 32)),
             nn.ReLU(),
             layer_init(nn.Linear(32, 64)),
@@ -23,19 +23,26 @@ class MLPAgent(nn.Module):
         )
         self.action_space_dims = envs.single_action_space.nvec
         self.actor = layer_init(nn.Linear(64, sum(self.action_space_dims)), std=0.01)
+
+        # TODO Modify with GNN (centralized critic MAPPO)
+        # Current implementation is IPPO (no centralized critic)
+        self.value_latent = nn.Sequential(
+            layer_init(nn.Linear(18, 32)),
+            nn.ReLU(),
+            layer_init(nn.Linear(32, 64)),
+            nn.ReLU(),
+        )
         self.critic = layer_init(nn.Linear(64, 1), std=1)
 
     def get_value(self, x):
-        # TODO Modify with GNN (centralized critic MAPPO)
-        # Current implementation is IPPO (no centralized critic)
         x = x.clone()
-        return self.critic(self.network(x))
+        return self.critic(self.value_latent(x))
 
     def get_action_and_value(self, x, action=None, use_mode: bool = False):
         # Input shape: (..., feature_size)
         # Modified to support MultiDiscrete
         x = x.clone()
-        hidden = self.network(x)
+        hidden = self.actor_latent(x)
         logits = self.actor(hidden)
         hdg_logits, alt_logits, spd_logits = logits.split(tuple(self.action_space_dims), dim=-1)
         hdg_probs = Categorical(logits=hdg_logits)
@@ -48,4 +55,4 @@ class MLPAgent(nn.Module):
                 action = torch.stack((hdg_probs.sample(), alt_probs.sample(), spd_probs.sample()))
         log_prob = hdg_probs.log_prob(action[0]) + alt_probs.log_prob(action[1]) + spd_probs.log_prob(action[2])
         entropy = hdg_probs.entropy() + alt_probs.entropy() + spd_probs.entropy()
-        return action, log_prob, entropy, self.critic(hidden)
+        return action, log_prob, entropy, self.critic(self.value_latent(x))
