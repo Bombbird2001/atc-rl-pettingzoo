0b6df095498b096a02c2db93f40fa61daa22a12c
Shang Yi Ku <kushangyi2001@gmail.com>
2026-02-22 18:36:14 +0800

    Also update cluster training script to take conflict penalties as script arguments

--- cluster_training_launcher.py
+++ cluster_training_launcher.py
@@ -6,10 +6,21 @@ import subprocess
 import sys
 import time
 from math import ceil
+from typing import List
 
 
 ENVS_PER_CPU = 2
 SIMULATOR_JAR = os.getenv("SIMULATOR_JAR")
+
+
+def get_arg(arg_name: str, input_args: List[str]) -> str:
+    envs_flag = list(filter(lambda x: arg_name == x[1], enumerate(input_args)))
+    if len(envs_flag) == 0:
+        raise Exception(f"Expected {arg_name} flag")
+    if len(envs_flag) > 1:
+        raise Exception(f"Expected only a single {arg_name} flag")
+
+    return input_args[envs_flag[0][0] + 1]
 
 
 if __name__ == "__main__":
@@ -24,16 +35,16 @@ if __name__ == "__main__":
     script_to_invoke = sys.argv[1]
     script_args = sys.argv[2:]
 
-    envs_flag = list(filter(lambda x: "--num-envs" in x[1], enumerate(script_args)))
-    if len(envs_flag) == 0:
-        raise Exception("Expected --num-envs flag")
-    if len(envs_flag) > 1:
-        raise Exception("Expected only a single --num-envs flag")
-    envs_to_start = int(sys.argv[envs_flag[0][0] + 3])
+    envs_to_start = int(get_arg("--num-envs", script_args))
     if assigned_cores is not None:
         required_cores = ceil(envs_to_start / ENVS_PER_CPU)
         if required_cores > len(assigned_cores):
             raise Exception(f"Requires {required_cores} cores for {envs_to_start} envs")
+
+    # Get reward penalties to pass to simulator processes
+    mva_penalty = float(get_arg("--mva-penalty", script_args))
+    conflict_penalty = float(get_arg("--conflict-penalty", script_args))
+    wake_penalty = float(get_arg("--wake-penalty", script_args))
 
     # Start trainer process first, pin to first core if possible
     env_ids = [f"{env_no}_{random.randbytes(3).hex()}" for env_no in range(envs_to_start)]
@@ -59,7 +70,7 @@ if __name__ == "__main__":
         taskset_args = []
         if assigned_cores:
             taskset_args.extend(["taskset", "-c", str(assigned_cores[(idx // ENVS_PER_CPU) + 1])])
-        all_args = taskset_args + sim_args + [env_id, "0"]
+        all_args = taskset_args + sim_args + [env_id, "0", str(mva_penalty), str(conflict_penalty), str(wake_penalty)]
         print("Launching", " ".join(all_args))
         env_processes.append(subprocess.Popen(all_args))
 
